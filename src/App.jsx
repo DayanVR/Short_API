19c44c2207266d726d0713904f3024b3e5290bce
DayanVR <108312281+DayanVR@users.noreply.github.com>
2024-05-03 14:51:40 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-4xl bg-black py-8 text-red-600 text-center">Welcome to Vite with Tailwinds and React</h1>  
+    </>
+  );
+}
+
+export default App;
